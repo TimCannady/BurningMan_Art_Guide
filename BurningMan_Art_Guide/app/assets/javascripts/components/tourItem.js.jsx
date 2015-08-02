1d4c44aad5cc21c6cfef4e58a1ed63884a60c9ca
Tim Cannady <timcannady@gmail.com>
2015-08-02 15:58:34 -0700

    some small css formatting

--- BurningMan_Art_Guide/app/assets/javascripts/components/tourItem.js.jsx
+++ BurningMan_Art_Guide/app/assets/javascripts/components/tourItem.js.jsx
@@ -2,9 +2,9 @@ var TourItem = React.createClass({
 
   render: function(){
     return (
-      <div className="installation_wrapper">
-        <h4>{this.props.installation_name}</h4>
-        <img src={this.props.photo_url} className="installation_img"></img>
+      <div className="tour_item_wrapper">
+        <h5>{this.props.installation_name}</h5>
+        <img src={this.props.photo_url} className="tour_img"></img>
       </div>
       )
   }
