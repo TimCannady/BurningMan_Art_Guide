5c401d6aaddff92da6c5d54da365bf82383d0e8a
Tim Cannady <timcannady@gmail.com>
2015-08-02 15:40:09 -0700

    Add to Tour Guide button working after fixing typo

--- BurningMan_Art_Guide/app/assets/javascripts/components/tourList.js.jsx
+++ BurningMan_Art_Guide/app/assets/javascripts/components/tourList.js.jsx
@@ -2,7 +2,7 @@ var TourList = React.createClass({
 
   render: function(){
     that = this;
-    var artInstallations = this.props.installations.map(function(installation,inded){
+    var artInstallations = this.props.installations.map(function(installation,index){
         return <TourItem key={index} installation_name={installation.installation_name} installation_description={installation.installation_description} artist_name={installation.artist_name} artist_location={installation.artist_location} url={installation.url} photo_url={installation.photo_url} email={installation.email} donate_link={installation.donate_link} addToTourGuide={that.addToTourGuide} />
     })
 
