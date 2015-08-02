ba12eab7204272ce01238b5039bd8aa668f1411a
Tim Cannady <timcannady@gmail.com>
2015-08-02 16:40:36 -0700

    add save-tour button. doesn't work

--- BurningMan_Art_Guide/app/assets/javascripts/components/tourList.js.jsx
+++ BurningMan_Art_Guide/app/assets/javascripts/components/tourList.js.jsx
@@ -8,7 +8,10 @@ var TourList = React.createClass({
 
     return(
       <div className="col-md-4 tour_box">
-        <h3>My Tour Guide</h3>
+        <div >
+          <h3>My Tour Guide</h3>
+          <a href="x">Save Tour</a>
+        </div>
         {artInstallations}
       </div>
       );
