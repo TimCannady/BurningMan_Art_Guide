4bf6ead6f917b5dd9b74dabff5cbf2bf2a39059b
Tim Cannady <timcannady@gmail.com>
2015-08-02 10:22:24 -0700

    successfully rendering all of an Installation_Items info, incl photo

--- BurningMan_Art_Guide/app/assets/javascripts/components/installationItem.js.jsx
+++ BurningMan_Art_Guide/app/assets/javascripts/components/installationItem.js.jsx
@@ -2,7 +2,19 @@ var InstallationItem = React.createClass({
   render: function(){
     return (
       <div>
-       {this.props.installations}
+       {/*{this.props.installations}*/}
+
+      <p>{this.props.installation_name}</p>
+      <img src={this.props.photo_url}></img>
+      <p>{this.props.photo_url}</p>
+      <p>{this.props.installation_description}</p>
+      <p>{this.props.artist_name}</p>
+      <p>{this.props.artist_location}</p>
+      <p>{this.props.url}</p>
+      <p>{this.props.email}</p>
+      <p>{this.props.donate_link}</p>
+
+
       </div>
       )
   }
--- BurningMan_Art_Guide/app/assets/javascripts/components/installationList.js.jsx
+++ BurningMan_Art_Guide/app/assets/javascripts/components/installationList.js.jsx
@@ -1,11 +1,16 @@
 var InstallationList = React.createClass({
   render: function(){
 
-    var artPieces =  this.props.installations
+    {/*var artInstallations =  this.props.installations*/}
+    artInstallations = this.props.installations.map(function(installation){
+        return <InstallationItem installation_name={installation.installation_name} installation_description={installation.installation_description} artist_name={installation.artist_name} artist_location={installation.artist_location} url={installation.url} photo_url={installation.photo_url} email={installation.email} donate_link={installation.donate_link} />
+    })
+
 
     return (
         <div className="installation_wrapper">
-          <InstallationItem installations={artPieces} />
+          {artInstallations}
+          {/*<InstallationItem installations={artInstallations} />*/}
         </div>
       );
   }
