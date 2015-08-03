0b4932b22f84e552194f7975a9c71e487ba06e6c
Tim Cannady <timcannady@gmail.com>
2015-08-03 11:50:27 -0700

    tidy

--- BurningMan_Art_Guide/app/assets/javascripts/components/tourList.js.jsx
+++ BurningMan_Art_Guide/app/assets/javascripts/components/tourList.js.jsx
@@ -7,7 +7,7 @@ var TourList = React.createClass({
   render: function(){
     that = this;
     var tourInstallations = this.props.installations.map(function(installation,index){
-        return <TourItem key={index} installation_name={installation.installation_name} installation_description={installation.installation_description} artist_name={installation.artist_name} artist_location={installation.artist_location} url={installation.url} photo_url={installation.photo_url} email={installation.email} donate_link={installation.donate_link}  />
+        return <TourItem key={index} installation_name={installation.installation_name} installation_description={installation.installation_description} artist_name={installation.artist_name} artist_location={installation.artist_location} url={installation.url} photo_url={installation.photo_url} email={installation.email} donate_link={installation.donate_link} />
     })
 
     return(
