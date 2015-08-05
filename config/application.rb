ff2e02b2eb8168833ab9f013d0e7724d86c80d6f
Tim Cannady <timcannady@gmail.com>
2015-08-05 05:50:54 -0700

    ap

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,6 @@ module BurningManArtGuide
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.serve_static_assets = true
+    config.serve_static_files = true
   end
 end
